13149ad176cc1558aa1d8209ddac73ba0fe819f8
Iago Anjos <iagokaued.com>
2026-02-08 13:57:09 -0300

    implementação de lógica para filtro de water e design

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const sunlightSelect = document.getElementById("sunlight");
+const waterSelect = document.getElementById("water");
 let allPlants = [];
 
 fetch("plants.json")
-  .then((response) => response.json())
-  .then((data) => {
-    allPlants = data;
-  });
+  .then((r) => r.json())
+  .then((data) => (allPlants = data))
+  .catch((e) => console.error(e));
 
 function filterPlants() {
   const sun = sunlightSelect.value;
